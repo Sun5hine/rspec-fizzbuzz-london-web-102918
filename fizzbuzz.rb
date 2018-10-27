767eefd7961364601fc69880d699ff54a71bc870
sun5hine <af5kh@yahoo.com>
2018-10-27 17:34:44 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,12 @@
  require "pry"
  
 def frizzbuzz(int)
+  
   if int % 3 == 0 
     "Fizz"
     elsif int % 5 == 0 
     "Buzz"
-    elsif int % 3 == 0 && int % 5 == 0 
-    "FrizzBuzz"
+    
   end
   binding.pry 
 end
